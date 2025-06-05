b0305026d612bff2d7382017db2867127214b4cb
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-04 23:07:59 -0300

    Resumo de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -24,6 +24,14 @@ export async function transactionsRoutes(app: FastifyInstance) {
     };
   });
 
+  app.get("/summary", async () => {
+    const summary = await knex("transaction")
+      .sum("amount", { as: "amount" })
+      .first();
+
+    return { summary };
+  });
+
   app.post("/", async (request, response) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
