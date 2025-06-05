48af08304ed9f76066bb8703bd907b96471e861e
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-04 22:36:04 -0300

    Criação de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,26 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { knex } from "../database";
+import { randomUUID } from "node:crypto";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transaction = await knex("transaction")
-      .where("amount", 1000)
-      .select("*");
+  app.post("/", async (request, response) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
 
-    return transaction;
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await knex("transaction").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
+
+    return response.status(201).send();
   });
 }
