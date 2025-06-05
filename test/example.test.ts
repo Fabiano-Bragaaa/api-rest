4ff8e6ae623199cd462667b239a6da69e6233158
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-05 14:14:58 -0300

    Testando criação de transação

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,3 +1,20 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, expect, test } from "vitest";
+import request from "supertest";
+import { app } from "../src/app";
 
-test("o usuario consegue criar uma nova transação", () => {});
+beforeAll(async () => {
+  await app.ready();
+});
+
+afterAll(async () => {
+  await app.close();
+});
+test("user can create a new transaction", async () => {
+  const response = await request(app.server).post("/transactions").send({
+    title: "new transaction",
+    amount: 500,
+    type: "credit",
+  });
+
+  expect(response.statusCode).toEqual(201);
+});
